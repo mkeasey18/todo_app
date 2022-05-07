0a34756581808f01ae9c54513047c87d26f0b7fa
mkeasey18 <m.keasey1803@gmail.com>
2022-05-07 15:34:19 -0400

    added docker files

--- app/todo_project/settings.py
+++ app/todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2zv0!i0y9a6x8-)u(h%@1(dxryl8-#epvo!1d&533u%t!^#+!x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_app'
 ]
 
 MIDDLEWARE = [
