80100122867fa712a7bc851cc28a4dfb5cdde2b4
mkeasey18 <m.keasey1803@gmail.com>
2022-05-10 08:00:53 -0400

    updated code to work with multiple docker containers

--- app/todo_app/models.py
+++ app/todo_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length=2000, default='')
+    date_created = models.DateField(default=datetime.date.today)
+    due_date = models.DateField(default=datetime.date.today)
+    STATUS_CHOICES = [
+        ('Done', 'Done'),
+        ('Ongoing', 'Ongoing')
+    ]
+    status = models.CharField(max_length = 20, choices=STATUS_CHOICES, default = 'Done')
